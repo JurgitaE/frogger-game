6e3f6db762d62e922789c5ce565e16d7c57edea8
Jurgita <jurgita.eicinaite@yahoo.com>
2023-06-15 22:20:06 +0300

    road and grass images

--- utilities.js
+++ utilities.js
@@ -1,8 +1,10 @@
 function animate() {
     ctx3.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.drawImage(background_lvl2, 0, 0, canvas.width, canvas.height);
     frogger.draw();
     frogger.update();
     handleObstacles();
+    ctx4.drawImage(grass, 0, 0, canvas.width, canvas.height);
     requestAnimationFrame(animate);
 }
 
