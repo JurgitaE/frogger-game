651d9fe57be49e6ab8ea1fd806c680db97925d0d
Jurgita <jurgita.eicinaite@yahoo.com>
2023-06-15 17:35:23 +0300

    lane 2-4 implementation

--- obstacles.js
+++ obstacles.js
@@ -8,10 +8,20 @@ class Obstacle {
         this.type = type;
     }
     draw() {
+        ctx1.fillStyle = 'blue';
         ctx1.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
         this.x += this.speed * gameSpeed;
+        if (this.speed > 0) {
+            if (this.x > canvas.width + this.width) {
+                this.x = 0 - this.width;
+            }
+        } else {
+            if (this.x < 0 - this.width) {
+                this.x = canvas.width + this.width;
+            }
+        }
     }
 }
 
@@ -19,7 +29,23 @@ function initObstacles() {
     // lane 1
     for (let i = 0; i < 2; i++) {
         let x = i * 350;
-        carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid, grid, 1, 'car'));
+        carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid * 2, grid, 1, 'car'));
+    }
+    // lane 2
+    for (let i = 0; i < 2; i++) {
+        let x = i * 300;
+        carsArray.push(new Obstacle(x, canvas.height - grid * 3 - 20, grid * 2, grid, -2, 'car'));
+    }
+    // lane 3
+    for (let i = 0; i < 2; i++) {
+        let x = i * 400;
+        carsArray.push(new Obstacle(x, canvas.height - 4 * grid, grid * 2, grid, 2, 'car'));
+    }
+
+    // lane 4
+    for (let i = 0; i < 2; i++) {
+        let x = i * 400;
+        logsArray.push(new Obstacle(x, canvas.height - 5 * grid, grid * 2, grid, -2, 'log'));
     }
 }
 initObstacles();
@@ -28,5 +54,11 @@ function handleObstacles() {
     for (let i = 0; i < carsArray.length; i++) {
         carsArray[i].draw();
         carsArray[i].update();
+        // logsArray[i].draw();
+        // logsArray[i].update();
+    }
+    for (let i = 0; i < logsArray.length; i++) {
+        logsArray[i].draw();
+        logsArray[i].update();
     }
 }
