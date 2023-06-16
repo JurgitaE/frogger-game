80e99dbe7090d151266a6c5d6ad48639486b1280
Jurgita <jurgita.eicinaite@yahoo.com>
2023-06-16 13:38:51 +0300

    game speed adjusted

--- obstacles.js
+++ obstacles.js
@@ -107,7 +107,7 @@ function handleObstacles() {
 
         for (let i = 0; i < logsArray.length; i++) {
             if (collision(frogger, logsArray[i])) {
-                frogger.x += logsArray[i].speed;
+                frogger.x += logsArray[i].speed * gameSpeed;
                 safe = true;
             }
         }
